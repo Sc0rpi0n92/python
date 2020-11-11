7c7494d1fa6bbd9c83feca3cf5b3ef4f6c82e36f
Sc0rpi0n92 <73743889+Sc0rpi0n92@users.noreply.github.com>
2020-11-11 12:32:52 +0300

    Update 02_05.py

--- 02_05.py
+++ 02_05.py
@@ -3,19 +3,31 @@ print(f"Рейтинг - {list}")
 
 
 def check():
+    '''
+    С помощью функции допилил пятое задание урока 2:
+
+    5. Реализовать структуру «Рейтинг», представляющую собой не возрастающий набор натуральных чисел.
+    У пользователя необходимо запрашивать новый элемент рейтинга.
+    Если в рейтинге существуют элементы с одинаковыми значениями,
+    то новый элемент с тем же значением должен разместиться после них.
+    Реализовал ввод числа с проверкой через функцию.
+    Также изменил точку выхода на отрицательное значение.
+    Как здесь сделать выход через символ - понять не могу.
+    Буду рад помощи в ЛС
+    '''
     while True:
         try:
-            list_upd = int(input("Enter a number (111 to break): "))
+            list_upd = int(input("Enter a number (-1 to break): "))
             # print(type(list_upd))
             # print(list_upd)
             return list_upd
         except ValueError:
             print("NaN, try again: ")
 
-
+print(check.__doc__)
 while True:
     list_upd = check()
-    if list_upd == 111:
+    if list_upd == -1:
         break
     for el in range(len(list)):
         if list[el] == list_upd:
